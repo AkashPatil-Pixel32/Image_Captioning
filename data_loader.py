85b1e284b08326ce3b51386b27d4ed8f6f7f7e8a
Akash <akashpatil1023@gmail.com>
2020-10-17 17:19:34 -0400

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,8 +5,6 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
-# borrowed from http://pytorch.org/tutorials/advanced/neural_style_tutorial.html
-# and http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 # define a training image loader that specifies transforms on images. See documentation for more details.
 train_transformer = transforms.Compose([
     transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
